a770de9f69a85e6a9348deba4973338c0716f070
SudarshanUdash <sudremotae@gmail.com>
2020-01-06 12:21:14 +0545

    Two fragments for products display products and login created

--- app/src/main/java/com/sudarsanudash/hamrobazarreplica/MainActivity.java
+++ app/src/main/java/com/sudarsanudash/hamrobazarreplica/MainActivity.java
@@ -3,10 +3,12 @@ package com.sudarsanudash.hamrobazarreplica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Toast;
 
 import com.sudarsanudash.hamrobazarreplica.TermsandCondition.Posting;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     CheckBox cbTerms, cbSafety, cbPosting;
     Button btnAgreed;
+    String terms, safety,ad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,50 +30,72 @@ public class MainActivity extends AppCompatActivity {
         cbPosting = findViewById(R.id.cbPosting);
         btnAgreed = findViewById(R.id.btnAgreed);
 
-        cbTerms.setOnClickListener(new View.OnClickListener() {
+        cbTerms.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Terms.class));
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent intent=new Intent(getApplicationContext(),Terms.class);
+                startActivity(intent);
+                terms="checked";
             }
         });
 
-        cbSafety.setOnClickListener(new View.OnClickListener() {
+        cbSafety.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Safety.class));
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent intent=new Intent(getApplicationContext(), Safety.class);
+                startActivity(intent);
+                safety="checked";
+            }
+        });
+        cbPosting.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent intent=new Intent(getApplicationContext(),Posting.class);
+                startActivity(intent);
+                ad="checked";
             }
         });
 
-        cbPosting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Posting.class));
-            }
-        });
 
         btnAgreed.setOnClickListener(new View.OnClickListener() {
             @Override
-            //Validation for terms and condition
             public void onClick(View v) {
-                if (cbTerms.isChecked()) {
-                    if (cbSafety.isChecked()){
-                        if (cbPosting.isChecked()) {
-                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
-
-
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Please Read and agree all term and condition", Toast.LENGTH_SHORT).show();
-
-                        }
-
-
-
-                    }
-
-                }
+                Agree();
             }
-
-
         });
+    }
+
+    private void Agree() {
+        if (!cbTerms.isChecked())
+        {
+            Toast.makeText(this, "Terms is not checked", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (!cbSafety.isChecked())
+        {
+            Toast.makeText(this, "Safety is not checked", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if (!cbPosting.isChecked())
+        {
+            Toast.makeText(this, "AD is not checked", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            SharedPreferences sharedPreferences=getSharedPreferences("welcome",MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+
+           editor.putString("Terms", terms);
+            editor.putString("safety",safety);
+            editor.putString("ad",ad);
+            editor.commit();
+
+
+
+            Intent intent=new Intent(MainActivity.this,HamroBazarSplashScreen.class);
+            startActivity(intent);
+
+        }
+
+
     }}
